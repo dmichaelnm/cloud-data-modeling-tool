05395f111793197eaafb04fcfb5de7434b63389f
Dirk Michael <dirkmichaelnm@gmail.com>
2025-07-06 13:04:54 +0200

    Add GCP connection testing and token generation utilities
    - Introduced `testConnection` function for GCP service provider connection testing.
    - Implemented `getImpersonatedToken` to generate impersonated tokens using service account credentials.
    - Added new types (`TRequest`, `TCredentials`, etc.) for structured request and credential management.
    - Renamed `test` function to `testConnection` and updated logic to handle dynamic provider-based requests.

---
+++ backend/functions/src/types.ts
@@ -0,0 +1,37 @@
+/**
+ * Represents the credential configuration for accessing AWS resources.
+ */
+export type TCredentialsAWS = {
+  iamRoleARN: string;
+};
+
+/**
+ * Represents the credentials required to authenticate with Google Cloud Platform (GCP).
+ */
+export type TCredentialsGCP = {
+  serviceAccount: string;
+};
+
+/**
+ * Represents the credentials required to connect to a Snowflake database.
+ */
+export type TCredentialsSnowflake = {
+  account: string;
+  username: string;
+  password: string;
+};
+
+/**
+ * Represents a set of credentials which could belong to one of the supported cloud service providers or platforms.
+ * This is a union type that combines specific credential types for GCP, AWS, or Snowflake, allowing flexibility
+ * in handling various authentication mechanisms based on the selected provider/platform.
+ */
+export type TCredentials = TCredentialsGCP | TCredentialsAWS | TCredentialsSnowflake;
+
+/**
+ * Represents a request object containing provider details and credentials.
+ */
+export type TRequest = {
+  provider: string;
+  credentials: TCredentials;
+};
